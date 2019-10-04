7e3f0076fc1bb3b1597d335892ebcce5b984b789
Alex De Meo <alexander.demeo@ocsbstudent.ca>
2019-10-04 16:57:57 -0400

    finished num text field almost

--- NumTextFieldAlexD/main.lua
+++ NumTextFieldAlexD/main.lua
@@ -23,13 +23,17 @@ local correctObject
 local incorrectObject
 local numericField 
 local randomNumber1
+local correctAnswer 
 local randomNumber2
 local userAnswer
-local correctAnswer
 local checkmark
 local red_X
 local dingSound
 local buzzSound
+local youWin
+local gameOver
+local pointsCount = 0
+local livesCount = 3
 
 ----------------------------------------------------------------
 --Local Functions
@@ -44,6 +48,11 @@ local function AskQuestion()
 
 	-- create the question
 	questionObject.text = randomNumber1 .. "+" .. randomNumber2 .. "="	
+end
+
+local function HideCorrectObject() 
+	correctObject.isVisible = false
+	AskQuestion()
 end
 
 local function HideCorrect()
@@ -76,24 +85,56 @@ local function NumericFieldListener( event)
 		-- when the answer is submitted (enter Key gets pressede) set user input to users answer
 		userAnswer = tonumber(event.target.text)
 
+
 		-- if the users answer and the correct answer are the same then
 		if (userAnswer == correctAnswer) then
-		correctObject.isVisible = true
-		checkmark.isVisible = true
-		audio.play (dingSound)
-		timer.performWithDelay (2000, HideCorrect)
-		timer.performWithDelay(2000, HideCheckmark)
-		event.target.text = ""
+			if (pointsCount == 4) then
+				youWin.isVisible = true
+				pointsText.isVisible = false
+				livesText.isVisible = false
+				numericField.isVisible = false
+				questionObject.isVisible = false
+				audio.play (dingSound)
+			else
+				correctAnswer = tostring(correctAnswer)
+				correctObject.isVisible = true
+				checkmark.isVisible = true
+				audio.play (dingSound)
+				pointsCount = pointsCount + 1
+				pointsText.text = "Points: " .. pointsCount
+				timer.performWithDelay (2000, HideCorrect)
+				timer.performWithDelay(2000, HideCheckmark)
+				event.target.text = ""
+			end
 		else 
-		incorrectObject.isVisible = true
-		red_X.isVisible = true
-		audio.play (buzzSound)
-		timer.performWithDelay (2000, HideRed_X)
-		timer.performWithDelay (2000, HideIncorrect)
-		event.target.text = ""
+			if (livesCount == 0) then 
+				gameOver.isVisible = true
+				pointsText.isVisible = false
+				livesText.isVisible = false
+				numericField.isVisible = false
+				questionObject.isVisible = false
+				audio.play (buzzSound)
+			else
+			
+				incorrectObject.isVisible = true
+				red_X.isVisible = true
+				audio.play (buzzSound)
+				livesCount = livesCount - 1
+				correctObject.text = "The right answer is " .. correctAnswer
+				correctObject.isVisible = true
+				correctObject:setTextColor (230/255, 0, 0)
+				correctObject.y = correctObject.y + 30		
+				livesText.text = "lives remaining: " .. livesCount
+				timer.performWithDelay (2000, HideRed_X)
+				timer.performWithDelay (2000, HideIncorrect)
+				timer.performWithDelay (2000, HideCorrectObject)
+				event.target.text = ""
+			end
 		end
 	end			
 end
+
+
 
 ------------------------------------------------------------------
 --OBJECT CREATION
@@ -108,10 +149,39 @@ correctObject = display.newText ("Correct!", display.contentWidth/2, display.con
 correctObject:setTextColor (0, 204/255, 0)
 correctObject.isVisible = false
 
--- create the incorrect object and make it invisible
-incorrectObject = display.newText ("Not Quite!", display.contentWidth/2, display.contentHeight*2/3, Arial, 50)
+-- create the incorrect object and make it invisible 
+incorrectObject = display.newText ("     Not Quite! \n " , display.contentWidth/2, display.contentHeight*2/3, Arial, 50)
 incorrectObject:setTextColor (230/255, 0, 0)
 incorrectObject.isVisible = false
+
+--create points text 
+pointsText = display.newText ("Points: " .. pointsCount, 100, 50, Arial, 40)
+pointsText:setTextColor (0, 0 ,0)
+
+-- create lives text 
+livesText = display.newText ("lives remaining: ".. livesCount, 178, 100, Arial, 40)
+livesText:setTextColor (0, 0, 0)
+
+--  create livesCount
+livesCount = 3
+
+-- create correctAnswer
+correctAnswer = 0
+
+-- create pointsCount
+pointsCount = 0
+
+-- create game over 
+gameOver = display.newImageRect ("Images/gameOver.png", 800, 700)
+gameOver.x = display.contentWidth/2
+gameOver.y = display.contentHeight/3
+gameOver.isVisible = false
+
+-- create you youWin
+youWin = display.newImageRect ("Images/youWin.png", 800, 400)
+youWin.x = display.contentWidth/2 + 30
+youWin.y = display.contentHeight/3
+youWin.isVisible = false
 
 --create the checkmark
 checkmark = display.newImageRect ("Images/checkmark.png", 150, 150)
@@ -122,7 +192,7 @@ checkmark.isVisible = false
 --create the red_X
 red_X = display.newImageRect ("Images/red_x.png", 150, 150)
 red_X.x = display.contentWidth*2/3
-red_X.y = display.contentHeight*2/3
+red_X.y = display.contentHeight/3
 red_X.isVisible = false
 
 --create the audio for the correct
