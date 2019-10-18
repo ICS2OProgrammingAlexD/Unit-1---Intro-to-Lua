61c9f28b642498742dd36895b2b9bb6502cb4e4f
Alex De Meo <alexander.demeo@ocsbstudent.ca>
2019-10-18 14:30:17 -0400

    finished Physics and collisions

--- PhysicsCollisionsAlex/main.lua
+++ PhysicsCollisionsAlex/main.lua
@@ -27,6 +27,8 @@ local ball3
 local ball4
 local beam1 
 local beam2
+local bkgMusic = audio.loadStream ("Sounds/formula-1-theme-song.mp3")
+local bkgMusicChannel
 
 ----------------------------------------------------
 --OBJECT CREATION
@@ -62,3 +64,61 @@ beam2.x = display.contentWidth*9/10
 beam2.y = display.contentCenterY*1.65
 -- rotate beam to be 135 degrees
 beam2:rotate(135)
+-- add physics
+physics.addBody(beam2, "static", {friction= 0.4, bounce= 0.3})
+
+----------------------------------------------------------
+-- LOCAL FUNCTIONS
+--------------------------------------------------------
+local function BKGMusic(event)
+	bkgMusicChannel = audio.play(bkgMusic, {loops= -1})
+end
+
+-- create ball1
+local function Ball1()
+	ball1 = display.newImage("Images/ball.png", 50, 50)
+	-- add physics
+	physics.addBody(ball1, {density= 1, friction= 0.7, bounce= 0.5, radius= 25})
+end
+
+-- create ball2 
+local function Ball2()
+	ball2 = display.newImage("Images/ball.png", 50, 50)
+
+	-- add physics
+	physics.addBody(ball2, {density= 0.75, friction= 0.3, bounce= 0.3, radius= 12.5})
+
+	-- scale ball2
+	ball2:scale (0.5, 0.5)
+end
+
+-- create ball3
+local function Ball3()
+	ball3 = display.newImage("Images/ball.png", 100, 100)
+
+	-- add physics
+	physics.addBody(ball3, {density = 0.2, friction= 0.2, bounce= 0.7, radius= 50})
+	ball3:scale (2, 2)
+end
+
+-- create ball4
+local function Ball4()
+	ball4 = display.newImage("Images/ball.png", 150, 150)
+
+	-- add physics
+	physics.addBody(ball4, {density= 0.01, friction= 0.01, bounce= 0.99, radius= 75})
+	ball4:scale(3, 3)
+end
+
+----------------------------------------------------------
+--TIMER DELAYS - FOR EACH FUNCTION
+-------------------------------------------------------------
+timer.performWithDelay(0, Ball1)
+timer.performWithDelay(500, Ball2)
+timer.performWithDelay(1350, Ball3)
+timer.performWithDelay(2200, Ball4)
+
+--------------------------------------------------------
+--FUNCTION CALLS
+---------------------------------------------------------
+Runtime:addEventListener("enterFrame", BKGMusic)
